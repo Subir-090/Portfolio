976ea6b9d3e000bd225bec8bda2723d0471f2f53
Subir Gupta <subirgupta090@gmail.com>
2023-07-07 17:14:48 +0530

    started work component

--- src/components/Work.js
+++ src/components/Work.js
@@ -3,76 +3,24 @@ import "./Work.css";
 const Work = () => {
   return (
     <div className="work">
-      <h2>Skills</h2>
-      {/* <ul className="skills">
-        <li className="skill">
-          <img
-            width="40"
-            height="40"
-            src="https://img.icons8.com/color/40/c-plus-plus-logo.png"
-            alt="c-plus-plus-logo"
-          />
-          C++
-        </li>
-        <div className="skill">
-          <img
-            width="40"
-            height="40"
-            src="https://img.icons8.com/pulsar-color/40/javascript.png"
-            alt="javascript"
-          />
-          Javascript
+      <section className="skills">
+        <h2>Skills</h2>
+        <div className="list">
+          <ul className="sub-lists">
+            <li className="skill">C++</li>
+            <li className="skill">Javascript</li>
+            <li className="skill">React Js</li>
+          </ul>
+          <ul className="sub-lists">
+            <li className="skill">Node Js</li>
+            <li className="skill">HTML</li>
+          </ul>
+          <ul className="sub-lists">
+            <li className="skill">CSS</li>
+            <li className="skill">jQuery</li>
+          </ul>
         </div>
-        <div className="skill">
-          <img
-            width="39"
-            height="39"
-            src="https://img.icons8.com/dotty/39/react.png"
-            alt="react"
-          />
-          React Js
-        </div>
-        <div className="skill">
-          <img
-            width="40"
-            height="40"
-            src="https://img.icons8.com/fluency/40/node-js.png"
-            alt="node-js"
-          />
-          Node Js
-        </div>
-        <div className="skill">
-          <img
-            width="40"
-            height="40"
-            src="https://img.icons8.com/external-tal-revivo-green-tal-revivo/40/external-mongodb-a-cross-platform-document-oriented-database-program-logo-green-tal-revivo.png"
-            alt="external-mongodb-a-cross-platform-document-oriented-database-program-logo-green-tal-revivo"
-          />
-          MongoDB
-        </div>
-        <div className="skill">
-          <img
-            width="40"
-            height="40"
-            src="https://img.icons8.com/arcade/40/sql.png"
-            alt="sql"
-          />
-          SQL
-        </div>
-      </ul> */}
-      <ul className="skills">
-        <li className="skill"><img
-            width="40"
-            height="40"
-            src="https://img.icons8.com/color/40/c-plus-plus-logo.png"
-            alt="c-plus-plus-logo"
-          />
-          C++</li>
-        <li className="skill"></li>
-        <li className="skill"></li>
-        <li className="skill"></li>
-        <li className="skill"></li>
-      </ul>
+      </section>
     </div>
   );
 };
